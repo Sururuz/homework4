ab22e4e65121e7a11b016f72d1cc72a44ba7550e
Sururuz <49961690+Sururuz@users.noreply.github.com>
2024-07-14 20:36:54 +0300

    Создал переменную и присвоил ей значение строки с произвольным текстом. Вывел количество символов введённого текста Вывел строку в верхнем регистре. Вывел строку в нижнем регистре. Изменил строку my_string, удалив все пробелы. Вывел первый символ строки Вывел последний символ строки

---
+++ homework4.py
@@ -0,0 +1,8 @@
+your_name = input("What's your name and lastname: ")
+print(len(your_name))
+
+print(your_name.upper())
+print(your_name.lower())
+print(your_name.replace(" ", ""))
+print(your_name[0])
+print(your_name[-1])
